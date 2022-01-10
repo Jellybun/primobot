5bb0df3ce576ee7b6fdd6d41eceff38bafcc2bc7
Jelly <jelly@AMEXs-MacBook-Pro.local>
2022-01-10 19:08:26 +0800

    test

--- cogs/events.py
+++ cogs/events.py
@@ -22,7 +22,7 @@ class Events(commands.Cog):
         welcomeChannel = self.client.get_channel(927195673684750336)
         welcomeMessage = await welcomeChannel.fetch_message(927256713999040532)
         embed = discord.Embed(description=f'Welcome to our server! {member.mention}\nYou are our {member.guild.member_count}th member', color=16777215)
-        embed.set_image(url='https://cdn.discordapp.com/attachments/926000918149873674/927479791832170506/Screen_Shot_2021-12-30_at_19.22.44.png')
+        embed.set_image(url='https://cdn.discordapp.com/attachments/832245157889441855/930055311887323206/Screen_Shot_2021-12-30_at_19.22.44.png')
         embed.set_author(name=member.name, icon_url=member.avatar_url)
         embed.set_footer(text=timeNow)
         profilechecker(member.id)
